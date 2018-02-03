9cf078157d7ec0b134af3eadfd066f696c4b561f
Ashish Gupta <ashishkrgupta30@gmail.com>
2018-02-03 12:11:33 +0530

    set name and emaillll

--- index.php
+++ index.php
@@ -6,16 +6,4 @@
  * --------------------------------------------
  * require __DIR__ . '/app/bootstrap.php';
  * $params = $_SERVER;
- * $params[\Magento\Store\Model\StoreManager::PARAM_RUN_CODE] = 'website2';
- * $params[\Magento\Store\Model\StoreManager::PARAM_RUN_TYPE] = 'website';
- * $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $params);
- * \/** @var \Magento\Framework\App\Http $app *\/
- * $app = $bootstrap->createApplication('Magento\Framework\App\Http');
- * $bootstrap->run($app);
- * --------------------------------------------
- *
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-
+ jkdnfndndnfn
